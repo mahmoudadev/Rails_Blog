dde2b2400cdb8dfe4ef5808d5a68f62eb611c1a7
Mahmoud <mahmoud.abdelmoneim.dev@gmail.com>
2018-07-21 00:38:52 +0200

    refacte post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,7 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:edit, :show, :destroy, :update]
+
+
 
  def index
    @posts = Post.all
@@ -9,7 +12,6 @@ class PostsController < ApplicationController
   end
 
   def edit
-    @post = Post.find(params[:id])
   end
 
   def create
@@ -24,7 +26,6 @@ class PostsController < ApplicationController
   end
 
   def update
-    @post = Post.find(params[:id])
     if @post.update(post_params)
       flash[:notice] = "Post is getting updated"
       redirect_to post_path(@post)
@@ -34,16 +35,17 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
   end
 
  def destroy
-   @post = Post.find(params[:id])
    @post.destroy
    redirect_to posts_path
  end
 
   private
+ def set_post
+   @post = Post.find(params[:id])
+ end
   def post_params
     params.require(:post).permit(:title, :body)
   end
