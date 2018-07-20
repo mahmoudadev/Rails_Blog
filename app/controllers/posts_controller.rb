65a338ecef49eb6268a2c5ddc5a00a1e9d2a50d7
Mahmoud <mahmoud.abdelmoneim.dev@gmail.com>
2018-07-20 18:36:54 +0200

    create listing posts and routing links

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,16 @@
 class PostsController < ApplicationController
-      def new
-        @post = Post.new
-      end
 
-      def edit
-        @post = Post.find(params[:id])
-      end
+ def index
+   @posts = Post.all
+ end
+
+  def new
+    @post = Post.new
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
 
   def create
     #render plain: params[:post].pretty_inspect
@@ -18,15 +23,15 @@ class PostsController < ApplicationController
     end
   end
 
-   def update
-     @post = Post.find(params[:id])
-     if @post.update(post_params)
-       flash[:notice] = "Post is getting updated"
-       redirect_to post_path(@post)
-     else
-       render 'edit'
-     end
-   end
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      flash[:notice] = "Post is getting updated"
+      redirect_to post_path(@post)
+    else
+      render 'edit'
+    end
+  end
 
   def show
     @post = Post.find(params[:id])
