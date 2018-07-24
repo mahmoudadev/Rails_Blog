0891c6fa2225c06c48b62ec4bf926b086bc7c2b4
Mahmoud <mahmoud.abdelmoneim.dev@gmail.com>
2018-07-24 15:20:06 +0200

    add sign in upon sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,8 +8,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:success] = 'Congrats!, you has been signed up successfully'
-      redirect_to posts_path
+      redirect_to user_path(@user)
     else
       render 'new'
     end
